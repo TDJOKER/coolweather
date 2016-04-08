c68e606200520c238cf585837853a2db46ffd170
TDJoker <zhaoziqinxc@163.com>
2016-04-08 16:56:58 +0800

    增加后台自动更新天气的功能

---
+++ src/com/coolweather/app/service/AutoUpdateService.java
@@ -0,0 +1,67 @@
+package com.coolweather.app.service;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
+
+import com.coolweather.app.receiver.AutoUpdateReceiver;
+import com.coolweather.app.util.HttpCallbackListener;
+import com.coolweather.app.util.HttpUtil;
+import com.coolweather.app.util.Utility;
+
+public class AutoUpdateService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				updateWeather();
+			}
+		}).start();
+		AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		int anHour = 8 * 60 * 60 * 1000;// 8个小时更新
+		long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+		Intent i = new Intent(this, AutoUpdateReceiver.class);
+		PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
+		manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	/**
+	 * 更新天气信息
+	 */
+	protected void updateWeather() {
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		String weatherCode = prefs.getString("weather_code", "");
+		String address = "http://www.weather.com.cn/data/cityinfo/"
+				+ weatherCode + ".html";
+		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+
+			@Override
+			public void onError(Exception e) {
+				e.printStackTrace();
+			}
+
+			@Override
+			public void OnFinished(String response) {
+				// 处理服务器返回的天气信息
+				Utility.handleWeatherResponse(AutoUpdateService.this, response);
+			}
+		});
+	}
+
+}
